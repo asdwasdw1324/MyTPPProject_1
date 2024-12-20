0442d92ea5f9b20a124f7ec7817e14bdfceb0a0b
Frank、Yang <278586884@qq.com>
2024-12-20 22:16:51 +0800

    2024/12/20

--- Source/MyTPPProject/Private/AbilitySystem/Abilities/WuKongGameplayAbility.cpp
+++ Source/MyTPPProject/Private/AbilitySystem/Abilities/WuKongGameplayAbility.cpp
@@ -3,7 +3,6 @@
 
 #include "AbilitySystem/Abilities/WuKongGameplayAbility.h"
 #include "AbilitySystem/WuKongAbilitySystemComponent.h"
-#include "DSP/Delay.h"
 
 void UWuKongGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec)
 {
@@ -14,37 +13,47 @@ void UWuKongGameplayAbility::OnGiveAbility(const FGameplayAbilityActorInfo* Acto
 		if (ActorInfo && !Spec.IsActive())
 		{
 			bEnhancedAttackActivated = ActorInfo->AbilitySystemComponent->TryActivateAbility(Spec.Handle);
+			if (!bEnhancedAttackActivated)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Ability activation failed!"));
+				return;
+			}
+			else
+			{
+				RestoreEnhancedAttackTimerHandle = FTimerHandle();
+				ActorInfo->OwnerActor->GetWorld()->GetTimerManager().SetTimer(RestoreEnhancedAttackTimerHandle, [this, Handle = Spec.Handle, ActorInfo]()
+				{
+					EndAbility(Handle, ActorInfo, FGameplayAbilityActivationInfo(), true, false);
+				}, 5.0f, false);
+			}
 		}
-	}
-	
-	// 调用EndAbility函数
-	if (bEnhancedAttackActivated)
-	{
-		EndAbility(Spec.Handle, ActorInfo, FGameplayAbilityActivationInfo(), false, false);
 	}
 }
 
 void UWuKongGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
 {
-	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
-
-	if (ActorInfo)
+	GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, FString::Printf(TEXT("EnhancedAttackActivated?: %s"), bEnhancedAttackActivated ? TEXT("True") : TEXT("False")));
+	if (bEnhancedAttackActivated)
 	{
-		FTimerHandle RestoreEnhancedAttackTimerHandle;
-		ActorInfo->OwnerActor->GetWorld()->GetTimerManager().SetTimer(RestoreEnhancedAttackTimerHandle, [this, Handle, AbilitySystemComponent = ActorInfo->AbilitySystemComponent]()
+		Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+		//bEnhancedAttackActivated = false;
+		if (GetWorld()->GetTimerManager().IsTimerActive(RestoreEnhancedAttackTimerHandle) && ActorInfo->OwnerActor.IsValid())
 		{
-			AbilitySystemComponent->ClearAbility(Handle);
-			bEnhancedAttackActivated = false;
-			// 添加日志以确认EndAbility被调用的次数
-			GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, FString::Printf(TEXT("EndAbility called. Handle: %s"), *Handle.ToString()));
-			GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Orange, FString::Printf(TEXT("Clear Ability Completed!")));
-		}, 5.0f, false);  // 5秒后恢复
+			ActorInfo->OwnerActor->GetWorld()->GetTimerManager().ClearTimer(RestoreEnhancedAttackTimerHandle);
+		}
+		
+		ActorInfo->AbilitySystemComponent->ClearAbility(Handle);
+		bEnhancedAttackActivated = false;
+		// 添加日志以确认EndAbility被调用的次数
+		GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Orange, FString::Printf(TEXT("EndAbility called. Handle: %s"), *Handle.ToString()));
+		GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Orange, FString::Printf(TEXT("Clear Ability Completed!")));
 	}
 	else
 	{
-		GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Orange, FString::Printf(TEXT("Can not Clear Ability!")));
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Red, FString::Printf(TEXT("Can not Clear Ability!")));
+		}
 	}
-	
 }
 
 
--- Source/MyTPPProject/Public/AbilitySystem/Abilities/WuKongGameplayAbility.h
+++ Source/MyTPPProject/Public/AbilitySystem/Abilities/WuKongGameplayAbility.h
@@ -27,6 +27,7 @@ protected:
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 
 	bool bEnhancedAttackActivated = false;
+	FTimerHandle RestoreEnhancedAttackTimerHandle;
 	
 	UPROPERTY(EditDefaultsOnly, Category = "WuKongAbility")
 	EWuKongAbilityActivationPolicy AbilityActivationPolicy = EWuKongAbilityActivationPolicy::OnTriggered;
